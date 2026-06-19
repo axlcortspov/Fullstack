c07efee927154040aa2b55dcf31d960703c60474
axlcortspov <ax.cortesm@duocuc.cl>
2026-06-19 00:06:29 +0000

    eureka server añadido

--- api-gateway/src/main/java/cl/duoc/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/cl/duoc/api_gateway/ApiGatewayApplication.java
@@ -2,9 +2,10 @@ package cl.duoc.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = { SimpleDiscoveryClientAutoConfiguration.class })
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
